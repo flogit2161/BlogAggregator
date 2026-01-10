1623d7e649a762226dab4d499ee8e1be5e4bb41b
flogit2161 <floj.jourdain@gmail.com>
2026-01-10 21:25:26 +0100

    Config struct and methods, setting up for CLI commands

---
+++ internal/config/config.go
@@ -0,0 +1,65 @@
+package config
+
+import (
+	"encoding/json"
+	"errors"
+	"os"
+)
+
+type state struct {
+	cfg *Config
+}
+
+type Config struct {
+	DbURL           string `json:"db_url"`
+	CurrentUserName string `json:"current_user_name"`
+}
+
+func (c *Config) SetUser(name string) error {
+	c.CurrentUserName = name
+	path, err := getConfigFilePath()
+	if err != nil {
+		return errors.New("Could not get filepath")
+	}
+
+	data, err := json.Marshal(c)
+	if err != nil {
+		return errors.New("Could not Marshal the config")
+	}
+
+	err = os.WriteFile(path, data, 0o600)
+	if err != nil {
+		return errors.New("Could not write to file")
+	}
+
+	return err
+}
+
+func ReadConfig() (Config, error) {
+	filepath, err := getConfigFilePath()
+	if err != nil {
+		return Config{}, errors.New("Could not get filepath")
+	}
+
+	data, err := os.ReadFile(filepath)
+	if err != nil {
+		return Config{}, errors.New("Could not read file from filepath")
+	}
+
+	cfg := Config{}
+	err = json.Unmarshal(data, &cfg)
+	if err != nil {
+		return Config{}, errors.New("Could not Unmarshal file")
+	}
+	return cfg, nil
+
+}
+
+func getConfigFilePath() (string, error) {
+	path, err := os.UserHomeDir()
+	if err != nil {
+		return "", errors.New("Could not get user's home path")
+	}
+	filepath := path + "/.gatorconfig.json"
+	return filepath, nil
+}
--- main.go
+++ main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/flogit2161/BlogAggregator/internal/config"
+)
+
+func main() {
+	cfg, err := config.ReadConfig()
+	if err != nil {
+		fmt.Println("Could not read from config")
+	}
+	err = cfg.SetUser("florian")
+	if err != nil {
+		fmt.Println("Could not set user")
+	}
+	cfg, err = config.ReadConfig()
+	fmt.Println(cfg)
+}
